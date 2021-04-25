c334a9083d46eadb6fd0b073a16a0ee3d8986b02
zichengsaber <zzczzc.1221@gmail.com>
2021-04-25 23:45:44 +0800

    To be continuing

---
+++ grad_cam.py
@@ -0,0 +1,180 @@
+import torch
+import argparse
+import cv2
+import numpy as np
+import torch
+from torch.autograd import Function
+from torchvision import models, transforms
+
+class FeatureExtractor():
+    """ Class for extracting activations and
+    registering gradients from targetted intermediate layers """
+    def __init__(self, model, target_layers):
+        self.model = model
+        self.target_layers = target_layers
+        self.gradients = []
+
+    def save_gradient(self, grad):
+        self.gradients.append(grad)
+
+    def __call__(self, x):
+        outputs = []
+        self.gradients = []
+        for name, module in self.model._modules.items():
+            x = module(x)
+            if name in self.target_layers:
+                x.register_hook(self.save_gradient)
+                outputs += [x]
+        return outputs, x
+
+class ModelOutputs():
+    """ Class for making a forward pass, and getting:
+    1. The network output.
+    2. Activations from intermeddiate targetted layers.
+    3. Gradients from intermeddiate targetted layers. """
+
+    def __init__(self, model, feature_module, target_layers):
+        self.model = model
+        self.feature_module = feature_module
+        self.feature_extractor = FeatureExtractor(self.feature_module, target_layers)
+
+    def get_gradients(self):
+        return self.feature_extractor.gradients
+
+    def __call__(self, x):
+        target_activations = []
+        for name, module in self.model._modules.items():
+            if module == self.feature_module:
+                target_activations, x = self.feature_extractor(x)
+            elif "avgpool" in name.lower():
+                x = module(x)
+                x = x.view(x.size(0),-1)
+            else:
+                x = module(x)
+
+        return target_activations, x
+
+def preprocess_image(img):
+    normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                 std=[0.229, 0.224, 0.225])
+    preprocessing = transforms.Compose([
+        transforms.ToPILImage(),
+        transforms.Resize((224,224)),
+        transforms.ToTensor(), # 如果已经在[0~1] 之间则不变化
+        normalize,
+    ])
+    return preprocessing(img.copy()).unsqueeze(0)
+
+def show_cam_on_image(img, mask):
+    heatmap = cv2.applyColorMap(np.uint8(255 * mask), cv2.COLORMAP_JET)
+    heatmap = np.float32(heatmap) / 255
+    cam = heatmap + np.float32(img)
+    cam = cam / np.max(cam)
+    return np.uint8(255 * cam)
+
+class GradCam:
+    def __init__(self, model, feature_module, target_layer_names, use_cuda):
+        self.model = model
+        self.feature_module = feature_module
+        self.model.eval()
+        self.cuda = use_cuda
+        if self.cuda:
+            self.model = model.cuda()
+
+        self.extractor = ModelOutputs(self.model, self.feature_module, target_layer_names)
+
+    def forward(self, input_img):
+        return self.model(input_img)
+
+    def __call__(self, input_img, target_category=None):
+        if self.cuda:
+            input_img = input_img.cuda()
+
+        features, output = self.extractor(input_img)
+
+        if target_category == None:
+            target_category = np.argmax(output.cpu().data.numpy())
+
+        one_hot = np.zeros((1, output.size()[-1]), dtype=np.float32)
+        one_hot[0][target_category] = 1
+        one_hot = torch.from_numpy(one_hot).requires_grad_(True)
+        if self.cuda:
+            one_hot = one_hot.cuda()
+        
+        one_hot = torch.sum(one_hot * output)
+
+        self.feature_module.zero_grad()
+        self.model.zero_grad()
+        one_hot.backward(retain_graph=True)
+
+        grads_val = self.extractor.get_gradients()[-1].cpu().data.numpy()
+
+        target = features[-1]
+        target = target.cpu().data.numpy()[0, :]
+
+        weights = np.mean(grads_val, axis=(2, 3))[0, :]
+        cam = np.zeros(target.shape[1:], dtype=np.float32)
+
+        for i, w in enumerate(weights):
+            cam += w * target[i, :, :]
+
+        cam = np.maximum(cam, 0)
+        cam = cv2.resize(cam, input_img.shape[2:])
+        cam = cam - np.min(cam)
+        cam = cam / np.max(cam)
+        return cam
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--use-cuda', action='store_true', default=False,
+                        help='Use NVIDIA GPU acceleration')
+    parser.add_argument('--image-path', type=str, default='./examples/both.png',
+                        help='Input image path')
+    args = parser.parse_args()
+    args.use_cuda = args.use_cuda and torch.cuda.is_available()
+    if args.use_cuda:
+        print("Using GPU for acceleration")
+    else:
+        print("Using CPU for computation")
+
+    return args
+
+def deprocess_image(img):
+    img = img - np.mean(img)
+    img = img / (np.std(img) + 1e-5)
+    img = img * 0.1
+    img = img + 0.5
+    img = np.clip(img, 0, 1)
+    return np.uint8(img*255)
+
+if __name__ == '__main__':
+    """ python grad_cam.py <path_to_image>
+    1. Loads an image with opencv.
+    2. Preprocesses it for VGG19 and converts to a pytorch variable.
+    3. Makes a forward pass to find the category index with the highest score,
+    and computes intermediate activations.
+    Makes the visualization. """
+
+    args = get_args()
+
+    model = models.resnet50(pretrained=True)
+    grad_cam = GradCam(model=model, feature_module=model.layer4, \
+                       target_layer_names=["2"], use_cuda=args.use_cuda)
+
+    img = cv2.imread(args.image_path, 1)
+    # Opencv loads as BGR:
+    img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+    input_img = preprocess_image(img)
+
+    img = np.float32(img)/255 #
+    # If None, returns the map for the highest scoring category.
+    # Otherwise, targets the requested category.
+    target_category = None
+    grayscale_cam = grad_cam(input_img, target_category)
+
+    grayscale_cam = cv2.resize(grayscale_cam, (img.shape[1], img.shape[0]))
+    cam = show_cam_on_image(img, grayscale_cam)
+
+
+    cv2.imwrite("./image/cam.jpg", cam)
+
---
+++ readme.md
@@ -0,0 +1,9 @@
+# Grad CAM Pytorch
+
+### 展示
+
+![](./image/cam.jpg)
+
+### 介绍
+[Grad cam](https://arxiv.org/abs/1610.02391)
+To be continuing...
